8cabc3ab788082d18340eb46b270589351d6bd1d
cry0genic <cry0genic1103@gmail.com>
2021-03-22 13:18:10 +0530

    added users

--- users/views.py
+++ users/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import *
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form':form})
+
+@login_required
+def profile(request):
+
+    if request.method == "POST":
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        try:
+            if u_form.is_valid and p_form.is_valid:
+                u_form.save()
+                p_form.save()
+                messages.success(
+                    request, f"Your account has been successfully Updated!")
+                return redirect("profile")
+        except Exception as e:
+            messages.warning(request, f"{e}")
+            return redirect("profile")
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+    context = {"u_form": u_form, "p_form": p_form}
+    return render(request, "users/profile.html", context)
+
+#follow user    
--- winter_assignment/urls.py
+++ winter_assignment/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('users/', include('users.urls')),
 
 ]
