42ee6c13c8e863949d4b61886cd58ff86d506f1a
cry0genic <cry0genic1103@gmail.com>
2021-03-30 18:26:33 +0530

    typo

--- winter_assignment/settings.py
+++ winter_assignment/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-from djangoproject.keyconfig import Database, Secrets
+from winter_assignment.keyconfig import Database, Secrets
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
