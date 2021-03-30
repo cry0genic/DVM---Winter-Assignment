c6f78cd548f40e107e2f1734e51bf76d47101d26
cry0genic <cry0genic1103@gmail.com>
2021-03-30 18:41:58 +0530

    dockerize django app

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,4 +15,7 @@ echo "Making migrations and migrating the database. "
 python manage.py makemigrations main --noinput 
 python manage.py migrate --noinput 
 
+python manage.py collectstatic --noinput
+
+
 exec "$@"
